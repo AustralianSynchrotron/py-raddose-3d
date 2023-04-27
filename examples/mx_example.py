b0c8bc29a923a9c40b07a4b14410604bb41d7ac7
Francisco Hernandez Vivanco <hernandf@ansto.gov.au>
2023-04-27 12:42:44 +1000

    added mx example and additional optional parameters

---
+++ examples/mx_example.py
@@ -0,0 +1,40 @@
+"""
+This example shows how you can run RADDOSE-3D to simulate a macromolecular crystallography
+experiment where a crystal of insulin is exposed to a Gaussian profile X-ray beam for 50
+seconds with a 90 degrees rotation (example taken from the the main RADDOSE-3D repository)
+"""
+
+from raddose_3d.raddose3d import RadDose3D
+from raddose_3d.schemas.input import Beam, Crystal
+
+crystal = Crystal(
+    Type="Cuboid",
+    Dimensions=(100, 100, 100),
+    PixelsPerMicron=0.1,
+    AbsCoefCalc="RD3D",
+    UnitCell=(78.02, 78.02, 78.02),
+    NumMonomers=24,
+    NumResidues=51,
+    ProteinHeavyAtoms=("Zn", 0.333, "S", 6),
+    SolventHeavyConc=("P", 425),
+    SolventFraction=0.64,
+)
+
+beam = Beam(
+    Type="Gaussian",
+    Flux=2e12,
+    FWHM=(20, 70),
+    Energy=12.1,
+    Collimation=("Rectangular", 100, 100),
+)
+
+rad_dose_3d = RadDose3D(
+    sample_id="my_sample",
+    crystal=crystal,
+    beam=beam,
+    wedge=(0.0, 90.0),
+    exposure_time=50.0,
+)
+
+summary = rad_dose_3d.run()
+print(summary)
--- raddose_3d/schemas/input.py
+++ raddose_3d/schemas/input.py
@@ -26,33 +26,49 @@ def _convert_tuple_to_str(input: tuple) -> str:
 
 
 class Crystal(BaseModel):
-    type: str = Field(example="Cuboid")
-    dimensions: tuple[int, int, int] | str = Field(
+    Type: str = Field(example="Cuboid")
+    Dimensions: tuple[int, int, int] | str = Field(
         description="Dimensions of the crystal in X,Y,Z in micrometers.",
         example=(100, 80, 60),
     )
-    coefcalc: str = Field(example="exp")
-    pdb: str = Field(
-        description="Use a PDB for the crystal composition", example="1KMT"
-    )
 
     # Optional parameters
+    Pdb: str | None = Field(
+        description="Use a PDB for the crystal composition", example="1KMT"
+    )
+    Coefcalc: str | None = Field(example="exp")
     AbsCoefCalc: str | None = Field(
-        description="Tells RADDOSE-3D how to calculate the " "Absorption coefficients",
+        description="Tells RADDOSE-3D how to calculate the absorption coefficients",
         example="RD3D",
     )
-    NumMonomers: float | None = Field(example=8)
-    NumResidues: float | None = Field(example=153)
-    ProteinHeavyAtoms: tuple[str, float, str, float] | str | None = Field(
-        example=("Zn", 2, "S", 6)
+    NumMonomers: int | None = Field(
+        description="number of monomers in unit cell", example=8
     )
-    SolventHeavyConc: tuple[str, float] | str | None = Field(example=("P", 425))
+    NumResidues: int | None = Field(
+        descprition="number of residues per monomer", example=153
+    )
+    ProteinHeavyAtoms: tuple | str | None = Field(
+        description="Heavy atoms added to protein part of the "
+        "monomer, i.e. S, coordinated metals, Se in Se-Met"
+        "Note: If a sequence file is used, S does not need to be added",
+        example=("Zn", 2, "S", 6),
+    )
+    SolventHeavyConc: tuple | str | None = Field(
+        description="Concentration of elements in the solvent in mmol/l. "
+        "Oxygen and lighter elements should not be specified",
+        example=("P", 425),
+    )
     UnitCell: tuple[float, float, float] | str | None = Field(
-        example=(78.4, 78.4, 78.4)
+        description="Unit cell size: a, b, c", example=(78.4, 78.4, 78.4)
     )
-    SolventFraction: float | None = Field(example=0.6436)
+    SolventFraction: float | None = Field(
+        description="Fraction of the unit cell occupied by solvent", example=0.6436
+    )
+    PixelsPerMicron: float | None = Field(
+        description="The computational resolution", example=0.1
+    )
 
-    @validator("dimensions", each_item=True)
+    @validator("Dimensions", each_item=True)
     def convert_dimensions_to_str(cls, v):
         return _convert_tuple_to_str(v)
 
@@ -70,16 +86,16 @@ class Crystal(BaseModel):
 
 
 class Beam(BaseModel):
-    type: str = Field(description="Beam type", example="Gaussian")
-    flux: float = Field(description="Flux in units of photons per second")
+    Type: str = Field(description="Beam type", example="Gaussian")
+    Flux: float = Field(description="Flux in units of photons per second", example=2e12)
     FWHM: tuple[float, float] | str | None = Field(
         description="Full Width Half Maximum in micrometers, X and Y for a Gaussian beam. "
         "X=vertical and Y = horizontal for a horizontal goniometer, opposite for a "
         "vertical goniometer",
         example=(10, 10),
     )
-    energy: float = Field(description="Energy in units of keV")
-    collimation: tuple[str, float, float] | str | None = Field(
+    Energy: float = Field(description="Energy in units of keV", example=12.1)
+    Collimation: tuple[str, float, float] | str | None = Field(
         description="X/Y collimation of the beam in micrometers. "
         "X = vertical and Y = horizontal for a horizontal goniometer. "
         "Opposite for a vertical goniometer",
@@ -87,14 +103,14 @@ class Beam(BaseModel):
     )
 
     # Parameters used for experimental beams only
-    file: str | None = Field(
+    File: str | None = Field(
         description="Tell RADDOSE-3D the name of the file", example="beam.pgm"
     )
     PixelSize: str | None = Field(
         description="Specify the pixel size in microns", example="0.3027 0.2995"
     )
 
-    @validator("collimation", each_item=True)
+    @validator("Collimation", each_item=True)
     def convert_collimation_to_str(cls, v):
         return _convert_tuple_to_str(v)
 
@@ -106,8 +122,13 @@ class Beam(BaseModel):
 class RadDoseInput(BaseModel):
     crystal: Crystal
     beam: Beam
-    wedge: tuple[float, float] | str
-    exposuretime: float
+    wedge: tuple[float, float] | str = Field(
+        description="Start and End rotational angle of the crystal in degrees",
+        example=(0.0, 90.0),
+    )
+    exposuretime: float = Field(
+        description="Total time for entire angular range in seconds", example=50.0
+    )
 
     @validator("wedge", each_item=True)
     def convert_tuple_to_str(cls, v: tuple[int, int]) -> str:
